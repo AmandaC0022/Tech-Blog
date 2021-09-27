adee316dd63e440822fe91c966085e766af1944b
Amanda Morgan <AmandaC0022@gmail.com>
2021-09-27 18:47:32 -0500

    styled views

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const path = require('path');
 const express = require('express');
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-const session = require('express-session');
+// const session = require('express-session');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-    secret: process.env.SESSION_SECRET,
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+// const sess = {
+//     secret: process.env.SESSION_SECRET,
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//       db: sequelize
+//     })
+//   };
 
 // const {
 //     User,
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session(sess));
+// app.use(session(sess));
 
 app.use(routes);
 
